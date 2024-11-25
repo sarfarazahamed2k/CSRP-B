085bc3a267aa808b535f6e8443bd0708625d8dff
sarfarazahamedrizwanbasha <124761816+sarfarazahamedrizwanbasha@users.noreply.github.com>
2024-11-25 14:11:19 +1030

    Final Update

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,10 @@
+if [ -f /data/.env ]; then
+    source /data/.env
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
 switch_azure_account $S2_ID
 
 cd on-premise
@@ -5,6 +12,11 @@ cd on-premise
 ./Delete-AzureResources.sh
 
 switch_azure_account $S1_ID
+
+cd Blue
+./Cleanup-Wazuh-Azure.sh 
+
+cd ..
 
 cd ../cloud
 
@@ -31,5 +43,23 @@ cd ../Scenario-4
 cd ..
 
 ./Cleanup-MicrosoftEntraConnectUser.sh
+
+cd ..
+
+# Loop through all groups in Azure AD
+for groupId in $(az ad group list --query "[].id" -o tsv); do
+  # Get the source property of the group
+  groupSource=$(az ad group show --group $groupId --query "onPremisesSyncEnabled" -o tsv)
+
+  # Check if the source is 'Windows Server AD' (onPremisesSyncEnabled is True)
+  if [ "$groupSource" == "true" ]; then
+    echo "Deleting group with ID: $groupId (Source: Windows Server AD)"
+    az ad group delete --group $groupId
+  fi
+done
+
+cd Red
+
+./cleanup.sh
 
 cd ..
--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# docker run -it --name azure-cli-1 -v $(pwd):/data mcr.microsoft.com/azure-cli 
+# docker start -ai azure-cli-1
+
+# cd /data
+# find . -type f -name "*.sh" -exec chmod +x {} +
+
 # Load environment variables from the .env file
-if [ -f .env ]; then
-    source .env
+if [ -f /data/.env ]; then
+    source /data/.env
 else
     echo ".env file not found!"
     exit 1
@@ -12,13 +18,12 @@ fi
 login_to_azure() {
     local tenant_id=$1
     # echo "Logging in to tenant $tenant_id..."
-    az login --tenant $tenant_id
+    # az login --tenant $tenant_id
 
     if [ $? -eq 0 ]; then
         echo "Login successful for tenant $tenant_id"
     else
         echo "Login failed for tenant $tenant_id"
-        exit 1
     fi
 }
 
@@ -32,7 +37,6 @@ switch_azure_account() {
         echo "Switched to subscription $subscription_id"
     else
         echo "Failed to switch to subscription $subscription_id"
-        exit 1
     fi
 }
 
@@ -52,43 +56,28 @@ login_to_azure $T2_ID
 
 echo "Both accounts logged in successfully."
 
+switch_azure_account $S1_ID
+cd Red
+./attackervm.sh
+
+cd ..
+
+
 switch_azure_account $S2_ID
 
 cd on-premise
 ./Deploy-Infrastructure.sh
 ./Deploy-Domain.sh
 
-cd Scenario-1
-./Scenario-1.sh
-
-cd ../Scenario-2
-./Scenario-2.sh
-
-cd ../Scenario-4
-./Scenario-4.sh
-
-cd ..
-
-switch_azure_account $S1_ID
-
-cd ../cloud
-./MicrosoftEntraConnectUser.sh
-
-# Ask for user input
-read -p "Microsoft Entra Connect user is created. Do you want to proceed? (Y/N): " user_input
-
-# Convert input to uppercase for comparison
-user_input=$(echo "$user_input" | tr '[:lower:]' '[:upper:]')
-
-# Check if the input is "Y"
-if [ "$user_input" != "Y" ]; then
-    exit 0
-fi
-
-sleep 60
 
 cd Scenario-1
 ./Scenario-1.sh
+
+# cd ../../Red
+# ./VPN-Setup.sh
+
+# cd ../on-premise/Scenario-1/
+# ./Scenario-1-2.sh
 
 cd ../Scenario-2
 ./Scenario-2.sh
@@ -99,38 +88,129 @@ cd ../Scenario-3
 cd ../Scenario-4
 ./Scenario-4.sh
 
-cd ../CyberDeception
-./Decoy-Users.sh
-./InfiniteRecursion.sh
-./Deploy-Files.sh
+cd ..
 
-cd ../Blue
-./start-Sentinel.sh
+switch_azure_account $S1_ID
+
+cd ../cloud
+cd Scenario-1
+./Scenario-1.sh
+cd ..
+./MicrosoftEntraConnectUser.sh
 
 # Ask for user input
-read -p "Azure Sentinel workspace is created. Do you want to proceed? (Y/N): " user_input
+read -p "1.Microsoft Entra Connect, 2.WireGaurd 3.JohnStrand user on workstation-1 IE, 4. Disbale Tamper Protection. Do you want to proceed? (Y/N): " user_input
 
 # Convert input to uppercase for comparison
 user_input=$(echo "$user_input" | tr '[:lower:]' '[:upper:]')
 
 # Check if the input is "Y"
-if [ "$user_input" != "Y" ]; then
-    exit 0
+if [ "$user_input" = "Y" ]; then
+
+    # sleep 60
+
+    # ./Assign-EMS.sh
+    # switch_azure_account $S2_ID
+
+    # cd ../on-premise
+    # ./Auto-Intune-Enrollment.sh
+
+    # read -p "Intune Enrollment GPO. Do you want to proceed? (Y/N): " user_input
+
+    # cd ../cloud
+
+    # switch_azure_account $S1_ID
+
+    # cd Scenario-1
+    # ./Scenario-1.sh
+
+    cd Scenario-2
+    ./Scenario-2.sh
+
+    cd ../Scenario-3
+    ./Scenario-3.sh
+
+    cd ../Scenario-4
+    ./Scenario-4.sh
+
+    cd ../CyberDeception
+    ./Decoy-Users.sh
+    # ./InfiniteRecursion.sh
+    ./Deploy-Files.sh
+
+    cd ../Blue
+    # ./start-Monitor.sh
+    ./start-Sentinel.sh
+
+    # Ask for user input
+    read -p "Azure Sentinel workspace is created. Do you want to proceed? (Y/N): " user_input
+
+    # Convert input to uppercase for comparison
+    user_input=$(echo "$user_input" | tr '[:lower:]' '[:upper:]')
+
+    if [ "$user_input" = "Y" ]; then
+        ./start-Defender.sh
+        ./start-Monitor.sh
+
+        cd ../../on-premise/Blue
+        ./Wazuh-Azure.sh
+
+        switch_azure_account $S2_ID
+
+        cd ../CyberDeception
+        ./Decoy-Users.sh
+        ./Deploy-Files.sh
+        ./CreateVM-Portspoof.sh
+
+        cd ../Blue
+        ./Deploy-Wazuh.sh
+        # ./Deploy-Velociraptor.sh
+        # ./Setup-OpenEDRAgent.sh
+        # sleep 300
+
+        cd ../Scenario-1
+        ./StartCmdAsUser.sh
+
+        # cd ../Scenario-3
+        # ./StartCmdAsUser.sh
+
+
+        # Ask for user input
+        # read -p "Setup Done. Do you want to proceed? (Y/N): " user_input
+
+        # Convert input to uppercase for comparison
+        # user_input=$(echo "$user_input" | tr '[:lower:]' '[:upper:]')
+        user_input="Y"
+
+        if [ "$user_input" = "Y" ]; then
+            # ./start_capture_all_vms.sh
+
+            cd ../../Red
+            ./VPN-Setup.sh
+            switch_azure_account $S2_ID
+
+            ./Disable-RTM.sh
+
+            switch_azure_account $S1_ID
+
+            ./Setup-FrontDoor.sh
+            ./Setup-Files.sh
+            . ./Prepare-Scenario.sh
+            cd ..
+        else
+            echo "Red team simulation skipped."
+        fi
+        # cd on-premise 
+
+        # sleep 120
+        # ./Join-Domain.sh
+
+        # cd ..
+
+    else
+        echo "Skipping Defender, Monitor, and further deployments."
+    fi
+
+else
+    echo "Skipping further actions after Microsoft Entra Connect user creation."
 fi
-
-./start-Defender.sh
-./start-Monitor.sh
-
-cd ..
-
-switch_azure_account $S2_ID
-
-cd ../on-premise/CyberDeception
-./Decoy-Users.sh
-./Deploy-Files.sh
-
-cd ../Blue
-./Deploy-Wazuh.sh
-./Deploy-Velociraptor.sh
-./Setup-OpenEDRAgent.sh
-./start_capture_all_vms.sh
